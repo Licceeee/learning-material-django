38c087b88663ce2d9fe833be26ebece6883412a6
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-05-24 19:08:11 +0200

    views + templates for material app/test

--- project/material/views.py
+++ project/material/views.py
@@ -1,12 +1,68 @@
 from django.views.generic import ListView, DetailView
-from .models import Topic
 from django.utils.translation import ugettext_lazy as _
+from .models import (Topic, Category, SubCategory, Material)
+
+
+class CategoryListView(ListView):
+    template_name = 'material/categories.html'
+    model = Category
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Showcase Section Infos
+        context['title'] = _("Categories Overview")
+        # SEO
+        context['page_title'] = _("Categories Overview")
+        context['page_description'] = _("Overview of all Categories")
+        return context
+
+
+class CategoryDetailView(DetailView):
+    template_name = 'material/category.html'
+    model = Category
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Showcase Section Infos
+        context['title'] = _("Category")
+        # SEO
+        context['page_title'] = _("Category")
+        context['page_description'] = _("Category")
+        return context
+
+
+class SubCategoryDetailView(DetailView):
+    template_name = 'material/subcategory.html'
+    model = SubCategory
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Showcase Section Infos
+        context['title'] = _("SubCategory")
+        # SEO
+        context['page_title'] = _("SubCategory")
+        context['page_description'] = _("SubCategory")
+        return context
+
+
+class MaterialDetailView(DetailView):
+    template_name = 'material/material.html'
+    model = Material
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Showcase Section Infos
+        context['title'] = _("Material")
+        # SEO
+        context['page_title'] = _("Material")
+        context['page_description'] = _("Material")
+        return context
 
 
 class TopicListView(ListView):
     template_name = 'material/topics.html'
     model = Topic
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # Showcase Section Infos
@@ -20,7 +76,7 @@ class TopicListView(ListView):
 class TopicDetailView(DetailView):
     template_name = 'material/topic.html'
     model = Topic
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # Showcase Section Infos
