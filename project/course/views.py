b681db38897f34dd6c217b76d5fcadc6d7c00cef
Alicia Schonefeld <schonefeld.dev@gmail.com>
2022-01-28 20:35:39 +0100

    course detailview add context data

--- project/course/views.py
+++ project/course/views.py
@@ -10,3 +10,13 @@ class CourseListView(ListView):
 class CourseDetailView(DetailView):
     template_name = 'course/course-detail.html'
     model = Course
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Showcase Section Infos
+        context['title'] = "Course Overview"
+        context['percentage'] = self.object.count_videos
+        # SEO
+        context['page_title'] = "Course Overview"
+        context['page_description'] = "Overview of the course"
+        return context
