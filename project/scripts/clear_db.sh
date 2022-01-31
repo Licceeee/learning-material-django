84b6ade5eb2dc22b87f2e368bf302948c23d3c8d
Alicia Schonefeld (Tornode) <schonefeld.dev@gmail.com>
2022-01-31 18:50:19 +0100

    update script

--- project/scripts/clear_db.sh
+++ project/scripts/clear_db.sh
@@ -1,22 +1,24 @@
 
-# rm db.sqlite3
+rm db.sqlite3
 rm -rf course/migrations/*
 rm -rf user/migrations/*
 rm -rf workout/migrations/*
 rm -rf routine/migrations/*
 rm -rf core/migrations/*
 
+python manage.py migrate admin zero
+python manage.py migrate auth zero
+python manage.py migrate contenttypes zero
+python manage.py migrate sessions zero
+
+python manage.py makemigrations
 python manage.py makemigrations user
 python manage.py makemigrations core
 python manage.py makemigrations course
 python manage.py makemigrations workout
 python manage.py makemigrations routine
 
-python manage.py makemigrations
 python manage.py migrate
-# python manage.py createsuperuser
+python manage.py makemigrations
+python manage.py createsuperuser
 
-python manage.py migrate admin zero
-python manage.py migrate auth zero
-python manage.py migrate contenttypes zero
-python manage.py migrate sessions zero
