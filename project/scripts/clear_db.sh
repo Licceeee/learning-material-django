c80995392b6cfc9ae65e8d3dd24908795de1e803
Alicia Schonefeld <schonefeld.dev@gmail.com>
2022-01-31 12:44:57 +0100

    update clear db script

--- project/scripts/clear_db.sh
+++ project/scripts/clear_db.sh
@@ -1,20 +1,20 @@
 
 # rm db.sqlite3
 rm -rf course/migrations/*
+rm -rf user/migrations/*
 rm -rf workout/migrations/*
-# rm -rf material/migrations/*
 rm -rf routine/migrations/*
+rm -rf core/migrations/*
 
 python manage.py makemigrations user
 python manage.py makemigrations core
-
-# python manage.py makemigrations material
 python manage.py makemigrations course
 python manage.py makemigrations workout
 python manage.py makemigrations routine
 
+python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser
+# python manage.py createsuperuser
 
 python manage.py migrate admin zero
 python manage.py migrate auth zero
