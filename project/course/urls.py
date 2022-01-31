7aebb225fde02f129a856f6ea455fe05cced263a
Alicia Schonefeld <schonefeld.dev@gmail.com>
2022-01-31 10:54:24 +0100

    update urls course, add login required

--- project/course/urls.py
+++ project/course/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import (CourseListView, CourseDetailView)
+from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-    path('', CourseListView.as_view(), name='courses'),
+    path('', login_required(CourseListView.as_view()), name='course-list'),
     path('<int:pk>', CourseDetailView.as_view(),
          name='course-detail'),
 ]
